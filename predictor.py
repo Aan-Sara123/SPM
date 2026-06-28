b672c6e08322a8496333ce21f1e76654cd116d56
Nehajoshy123 <nehajoshy.pj@gmail.com>
2026-06-28 17:40:41 +0530

    Create predictor.py

---
+++ predictor.py
@@ -0,0 +1,26 @@
+from database import get_history_for_user
+from sklearn.linear_model import LinearRegression
+
+def predict_cpu(username):
+
+    history = get_history_for_user(username)
+    cpu_history = [row["cpu"] for row in history]
+
+    if len(cpu_history) < 6:
+        return None
+
+    window = 5
+
+    X = []
+    y = []
+
+    for i in range(len(cpu_history) - window):
+        X.append(cpu_history[i:i + window])
+        y.append(cpu_history[i + window])
+
+    model = LinearRegression()
+    model.fit(X, y)
+
+    prediction = model.predict([cpu_history[-window:]])
+
+    return float(prediction[0])
