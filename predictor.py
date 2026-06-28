6a9d2f5e9244cada7f67c5cf0d3d0da50c97bf4c
Nehajoshy123 <nehajoshy.pj@gmail.com>
2026-06-28 17:54:10 +0530

    Update predictor.py

--- predictor.py
+++ predictor.py
@@ -1,11 +1,13 @@
-from database import get_history_for_user
 from sklearn.linear_model import LinearRegression
+from database import get_history_for_user
 
 def predict_cpu(username):
 
     history = get_history_for_user(username)
+
     cpu_history = [row["cpu"] for row in history]
 
+    # Need at least 6 readings
     if len(cpu_history) < 6:
         return None
 
@@ -15,10 +17,11 @@ def predict_cpu(username):
     y = []
 
     for i in range(len(cpu_history) - window):
-        X.append(cpu_history[i:i + window])
-        y.append(cpu_history[i + window])
+        X.append(cpu_history[i:i+window])
+        y.append(cpu_history[i+window])
 
     model = LinearRegression()
+
     model.fit(X, y)
 
     prediction = model.predict([cpu_history[-window:]])
